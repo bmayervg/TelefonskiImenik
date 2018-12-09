eb0aea4873d25eb0055840da04571c60cda2c723
Bruno Mayer <mayerbrunovg@gmail.com>
2018-12-09 22:22:01 +0100

    Pretraga tele imenika

--- TelefonskiImenik/Forms/frmImenikPretraga.Designer.cs
+++ TelefonskiImenik/Forms/frmImenikPretraga.Designer.cs
@@ -28,21 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvRezultati = new System.Windows.Forms.DataGridView();
+            this.btnPretrazi = new System.Windows.Forms.Button();
+            this.Ulica = new System.Windows.Forms.Label();
+            this.tbPostanskiBroj = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRezultati)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPostanskiBroj);
+            this.groupBox1.Controls.Add(this.Ulica);
+            this.groupBox1.Controls.Add(this.btnPretrazi);
+            this.groupBox1.Location = new System.Drawing.Point(4, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(986, 225);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parametri pretrage";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExport);
+            this.groupBox2.Controls.Add(this.dgvRezultati);
+            this.groupBox2.Location = new System.Drawing.Point(4, 244);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(986, 387);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Rezultati pretrage";
+            // 
+            // dgvRezultati
+            // 
+            this.dgvRezultati.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRezultati.Location = new System.Drawing.Point(8, 53);
+            this.dgvRezultati.Name = "dgvRezultati";
+            this.dgvRezultati.Size = new System.Drawing.Size(978, 328);
+            this.dgvRezultati.TabIndex = 0;
+            // 
+            // btnPretrazi
+            // 
+            this.btnPretrazi.Location = new System.Drawing.Point(511, 107);
+            this.btnPretrazi.Name = "btnPretrazi";
+            this.btnPretrazi.Size = new System.Drawing.Size(160, 23);
+            this.btnPretrazi.TabIndex = 0;
+            this.btnPretrazi.Text = "Pretraži";
+            this.btnPretrazi.UseVisualStyleBackColor = true;
+            this.btnPretrazi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ulica
+            // 
+            this.Ulica.AutoSize = true;
+            this.Ulica.Location = new System.Drawing.Point(286, 22);
+            this.Ulica.Name = "Ulica";
+            this.Ulica.Size = new System.Drawing.Size(29, 13);
+            this.Ulica.TabIndex = 1;
+            this.Ulica.Text = "P.br.";
+            // 
+            // tbPostanskiBroj
+            // 
+            this.tbPostanskiBroj.Location = new System.Drawing.Point(321, 19);
+            this.tbPostanskiBroj.Name = "tbPostanskiBroj";
+            this.tbPostanskiBroj.Size = new System.Drawing.Size(61, 20);
+            this.tbPostanskiBroj.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Grad";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(8, 24);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(160, 23);
+            this.btnExport.TabIndex = 5;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(318, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ulica";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(318, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ime";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(64, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(318, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Prezime";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(64, 47);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(61, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Predbroj";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(131, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Tel. broj";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(182, 47);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(130, 20);
+            this.textBox6.TabIndex = 14;
             // 
             // frmImenikPretraga
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1002, 635);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "frmImenikPretraga";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Pretraga imenika";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRezultati)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPostanskiBroj;
+        private System.Windows.Forms.Label Ulica;
+        private System.Windows.Forms.Button btnPretrazi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvRezultati;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
--- TelefonskiImenik/Forms/frmImenikPretraga.cs
+++ TelefonskiImenik/Forms/frmImenikPretraga.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +19,80 @@ namespace TelefonskiImenik.Forms
         public frmImenikPretraga()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            HtmlNodeCollection nodes = new HtmlNodeCollection(null);
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://www.hakom.hr/default.aspx?id=8391");
+            request.Method = "POST";
+            request.ReadWriteTimeout = 5000;//200000;
+
+            request.UserAgent = "Mozilla / 5.0(Windows NT 10.0; WOW64; Trident / 7.0; rv: 11.0) like Gecko";
+            var postData = "brojTel:385916121237";
+            postData += "&_validacija:crna";
+            postData += "&_valid:crna";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "text/html; charset=utf-8";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            //if (!string.IsNullOrEmpty(proxyIP))
+            //{
+            //    WebProxy wp = new WebProxy(proxyIP);
+            //    wp.BypassProxyOnLocal = true;
+            //    request.Timeout = requestTimeout + 5000;
+            //    request.Proxy = wp;
+            //    request.KeepAlive = false;
+            //    request.ProtocolVersion = HttpVersion.Version10;
+            //    request.AllowWriteStreamBuffering = false;
+            //}
+            //CookieContainer cc = new CookieContainer();
+            //request.CookieContainer = cc;
+            //if (cookies != null && cookies.Count != 0)
+            //{
+            //    for (int ck = 0; ck < cookies.Count; ck++)
+            //    {
+            //        Cookie c = new Cookie();
+            //        c.Secure = cookies[ck].Secure;
+            //        c.Port = cookies[ck].Port;
+            //        c.Path = cookies[ck].Path;
+            //        c.Name = cookies[ck].Name;
+            //        c.HttpOnly = cookies[ck].HttpOnly;
+            //        c.Expires = cookies[ck].Expires;
+            //        c.Domain = cookies[ck].Domain;
+            //        c.Value = cookies[ck].Value;
+            //        c.Discard = cookies[ck].Discard;
+            //        c.CommentUri = cookies[ck].CommentUri;
+            //        c.Comment = cookies[ck].Comment;
+            //        c.Expired = cookies[ck].Expired;
+            //        c.Version = cookies[ck].Version;
+            //        cc.Add(c);
+            //    }
+            //}
+            string responseFromServer = "";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            if (dataStream != null)
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                reader.Dispose();
+            }
+
+            if (!string.IsNullOrEmpty(responseFromServer))
+            {
+
+            }
         }
     }
 }
